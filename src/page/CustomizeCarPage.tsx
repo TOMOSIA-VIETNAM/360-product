67fe94338ba593570627b577d3d265e1a1620902
Lộc Huỳnh (Tomosia) <loc.huynh@tomosia.com>
2025-04-23 09:30:59 +0700

    feat: add hotspot

--- src/page/CustomizeCarPage.tsx
+++ src/page/CustomizeCarPage.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect, useCallback } from "react";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
-import listCarData from '../data/listCar.json';
+import listCarData from "../data/listCar.json";
 import { useParams, useNavigate, useLocation } from "react-router-dom";
 import iconLogoBMW from "/icon_logo.svg";
 interface Color {
@@ -40,6 +40,33 @@ interface CI360Viewer {
   init: (container: HTMLElement, update?: boolean) => void;
 }
 
+interface HotspotPosition {
+  imageIndex: number;
+  xCoord: number;
+  yCoord: number;
+}
+
+interface HotspotVariantImage {
+  src: string;
+  alt: string;
+}
+
+interface HotspotVariant {
+  images?: HotspotVariantImage[];
+  description?: string;
+}
+
+interface HotspotPopupProps {
+  popupSelector?: string;
+}
+
+interface GurkhaHotspotConfig {
+  variant?: HotspotVariant;
+  popupProps?: HotspotPopupProps;
+  initialDimensions?: [number, number];
+  positions?: HotspotPosition[];
+}
+
 interface HotspotConfig {
   position: {
     x: number;
@@ -63,6 +90,7 @@ declare global {
       getActiveIndexByID: (id: string, orientation: string) => number | null;
       changeFolder: (folder: string, showIndex: number) => void;
       _viewers?: CI360Viewer[];
+      addHotspots: (id: string, config: GurkhaHotspotConfig[]) => void;
     };
   }
 }
@@ -77,30 +105,114 @@ const CustomizeCarPage = () => {
   const location = useLocation();
   const [carData, setCarData] = useState<Car | null>(null);
   const [linkFolder, setLinkFolder] = useState("");
-
+  const isBmwZ4 = nameId === "bmw_z4";
   // const adjustImageUrl = useCallback((url: string | undefined): string => {
   //   if (!url) return "";
   //   return url.replace(/\/customize\/assets\//g, "/assets/");
-  // }, []);
+  // }, [])
+
+  const removeHotspotPopupById = useCallback((popupId: string) => {
+    const popupElements = document.querySelectorAll(
+      `div[data-hotspot-popup-id="${popupId}"]`
+    );
+
+    if (popupElements.length === 0) {
+      return;
+    }
+
+    popupElements.forEach((element) => {
+      element.parentNode?.removeChild(element);
+    });
+  }, []);
+
+  const removeAllHotspotPopups = useCallback(() => {
+    const popupElements = document.querySelectorAll(
+      "div[data-hotspot-popup-id]"
+    );
+    popupElements.forEach((element) => {
+      const popupId = element.getAttribute("data-hotspot-popup-id");
+      if (popupId) {
+        removeHotspotPopupById(popupId);
+      }
+    });
+  }, [removeHotspotPopupById]);
+
+  const removeHotspotIconById = useCallback((iconId: string) => {
+    const iconElements = document.querySelectorAll(
+      `div[data-hotspot-icon-id="${iconId}"]`
+    );
+
+    if (iconElements.length === 0) {
+      return;
+    }
+
+    iconElements.forEach((element) => {
+      element.parentNode?.removeChild(element);
+    });
+  }, []);
+
+  const removeAllHotspotIcons = useCallback(() => {
+    const iconElements = document.querySelectorAll("div[data-hotspot-icon-id]");
+
+    iconElements.forEach((element) => {
+      const iconId = element.getAttribute("data-hotspot-icon-id");
+      if (iconId) {
+        removeHotspotIconById(iconId);
+      }
+    });
+  }, [removeHotspotIconById]);
+
+  const removeAllHotspots = useCallback(() => {
+    removeAllHotspotPopups();
+    removeAllHotspotIcons();
+  }, [removeAllHotspotPopups, removeAllHotspotIcons]);
+
+  const GURKHA_SUV_HOTSPOTS_CONFIG: GurkhaHotspotConfig[] = [
+    {
+      variant: {
+        images: [
+          {
+            src: "https://scaleflex.cloudimg.io/v7/demo/360-assets/AIR_SNORKEL_FINAL_JPG.png?vh=88bccb",
+            alt: "air snorkel",
+          },
+        ],
+        description:
+          "The snorkel gives the Gurkha an unmatched water-wading ability and ensures ample supply of fresh air for combustion.",
+      },
+      popupProps: { popupSelector: "air-intake-popup" },
+      initialDimensions: [1170, 662],
+      positions: [
+        { imageIndex: 6, xCoord: 527, yCoord: 430 },
+        { imageIndex: 7, xCoord: 457, yCoord: 430 },
+        { imageIndex: 8, xCoord: 407, yCoord: 430 },
+        { imageIndex: 9, xCoord: 337, yCoord: 430 },
+        { imageIndex: 10, xCoord: 301, yCoord: 430 },
+        { imageIndex: 11, xCoord: 301, yCoord: 430 },
+        { imageIndex: 12, xCoord: 281, yCoord: 430 },
+        { imageIndex: 13, xCoord: 251, yCoord: 430 },
+      ],
+    },
+  ];
 
   const toggleRightPanel = () => {
     setRightPanelOpen(!rightPanelOpen);
   };
 
-
   const handleColorSelect = useCallback(
     (color: Color) => {
+      removeAllHotspots();
       setSelectedColor(color);
     },
-    [linkFolder]
+    [linkFolder, removeAllHotspots]
   );
 
   const handleWheelSelect = useCallback(
     (wheel: Wheel) => {
+      removeAllHotspots();
       setSelectedWheel(wheel);
       window?.CI360?.changeFolder(linkFolder, 9);
     },
-    [linkFolder]
+    [linkFolder, removeAllHotspots]
   );
 
   useEffect(() => {
@@ -111,21 +223,19 @@ const CustomizeCarPage = () => {
           "https://cdn.jsdelivr.net/npm/tms-360@1.0.18/dist/tms-360.min.js";
         script.async = true;
         script.onload = () => {
-          console.log("Script loaded, initializing CI360");
           if (window.CI360) {
             window.CI360.init();
           }
         };
         document.body.appendChild(script);
       } else if (window.CI360) {
-        console.log("CI360 already exists, initializing");
         window.CI360.init();
+        console.log("CI360 initialized", window.CI360);
       }
     };
     loadScript();
     const carDataDetail = listCarData.find((car) => car.nameId === nameId);
     setCarData(carDataDetail as unknown as Car);
-
   }, []);
 
   useEffect(() => {
@@ -157,7 +267,12 @@ const CustomizeCarPage = () => {
 
     const initialFolderPath = `https://tms-360-product.s3.ap-southeast-1.amazonaws.com/upload/${carData.nameId}/${initialColor.name}/${initialWheel.name}/`;
     setLinkFolder(initialFolderPath);
-
+    setTimeout(() => {
+      const viewer = window.CI360?._viewers?.[0];
+      if (window.CI360 && viewer && viewer.container && isBmwZ4) {
+        window.CI360.addHotspots("gurkha-suv", GURKHA_SUV_HOTSPOTS_CONFIG);
+      }
+    }, 200);
   }, [carData, location.search, navigate]);
 
   useEffect(() => {
@@ -168,11 +283,17 @@ const CustomizeCarPage = () => {
   }, [carData, selectedColor, selectedWheel]);
 
   const updateDataFolder = function (folder: string) {
+    removeAllHotspots();
+
     const viewer = window?.CI360?._viewers?.[0];
     if (viewer) {
-      const activeImageX = viewer.activeImageX === 1 ? 0 : viewer.activeImageX - 1;
+      const activeImageX =
+        viewer.activeImageX === 1 ? 0 : viewer.activeImageX - 1;
       try {
         window?.CI360?.changeFolder(folder, activeImageX);
+        if (isBmwZ4) {
+          window?.CI360?.addHotspots("gurkha-suv", GURKHA_SUV_HOTSPOTS_CONFIG);
+        }
       } catch (error) {
         console.error("Error changing folder:", error);
       }
@@ -213,14 +334,14 @@ const CustomizeCarPage = () => {
   // Add a currency formatting function
   const formatCurrency = (price: string | number): string => {
     // Convert to number if it's a string
-    const numericPrice = typeof price === 'string' ? parseFloat(price) : price;
+    const numericPrice = typeof price === "string" ? parseFloat(price) : price;
 
     // Format with Japanese Yen symbol and thousand separators
-    return new Intl.NumberFormat('ja-JP', {
-      style: 'currency',
-      currency: 'JPY',
+    return new Intl.NumberFormat("ja-JP", {
+      style: "currency",
+      currency: "JPY",
       minimumFractionDigits: 0,
-      maximumFractionDigits: 0
+      maximumFractionDigits: 0,
     }).format(numericPrice);
   };
 
@@ -253,12 +374,7 @@ const CustomizeCarPage = () => {
               {carData?.name}
             </Typography> */}
           </Box>
-          <img
-            src={iconLogoBMW}
-            alt=""
-            width="65px"
-            height="42px"
-          />
+          <img src={iconLogoBMW} alt="" width="65px" height="42px" />
         </Box>
       </Box>
     </div>
@@ -301,9 +417,21 @@ const CustomizeCarPage = () => {
                 <img src={color.imageColor} alt={color.name} />
                 {selectedColor?.id === color.id && (
                   <div className={styles["color-selected-tick"]}>
-                    <svg width="24" height="24" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <circle cx="8" cy="8" r="8" fill="#262626"/>
-                      <path d="M5 8L7 10L11 6" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle cx="8" cy="8" r="8" fill="#262626" />
+                      <path
+                        d="M5 8L7 10L11 6"
+                        stroke="white"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
                     </svg>
                   </div>
                 )}
@@ -357,9 +485,21 @@ const CustomizeCarPage = () => {
                 <img src={wheel.imageWheel} alt="" />
                 {selectedWheel?.id === wheel.id && (
                   <div className={styles["color-selected-tick"]}>
-                    <svg width="24" height="24" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-                      <circle cx="8" cy="8" r="8" fill="#262626"/>
-                      <path d="M5 8L7 10L11 6" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    <svg
+                      width="24"
+                      height="24"
+                      viewBox="0 0 16 16"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <circle cx="8" cy="8" r="8" fill="#262626" />
+                      <path
+                        d="M5 8L7 10L11 6"
+                        stroke="white"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      />
                     </svg>
                   </div>
                 )}
@@ -404,19 +544,20 @@ const CustomizeCarPage = () => {
         </IconButton>
       </div>
       <div className={styles["image-container"]}>
-        {
-          linkFolder && (
-            <div
-              className="cloudimage-360"
-              id="gurkha-suv"
-              data-folder={linkFolder}
-              data-filename-x="img_{index}.jpg"
-              data-amount-x="36"
-              data-spin-reverse="true"
-              data-speed="50"
-              data-drag-speed="120"
-              data-full-screen="true"
-              data-hide-360-logo="true"
+        {linkFolder && (
+          <div
+            className="cloudimage-360"
+            id="gurkha-suv"
+            data-folder={linkFolder}
+            data-filename-x="img_{index}.jpg"
+            data-amount-x="36"
+            data-spin-reverse="true"
+            data-speed="50"
+            data-drag-speed="120"
+            data-full-screen="true"
+            data-hide-360-logo="true"
+            data-magnifier="2"
+            data-keys="true"
           ></div>
         )}
       </div>
@@ -459,6 +600,30 @@ const CustomizeCarPage = () => {
       </div>
     </div>
   );
+
+  useEffect(() => {
+    const checkHotspots = () => {
+      // Kiểm tra popups
+      const allPopups = document.querySelectorAll("[data-hotspot-popup-id]");
+      console.log("All elements with data-hotspot-popup-id:", allPopups);
+      allPopups.forEach((popup) => {
+        const id = popup.getAttribute("data-hotspot-popup-id");
+        console.log(`Found popup with ID: ${id}`, popup);
+      });
+
+      // Kiểm tra icons
+      const allIcons = document.querySelectorAll("[data-hotspot-icon-id]");
+      console.log("All elements with data-hotspot-icon-id:", allIcons);
+      allIcons.forEach((icon) => {
+        const id = icon.getAttribute("data-hotspot-icon-id");
+        console.log(`Found icon with ID: ${id}`, icon);
+      });
+    };
+
+    // Gọi hàm kiểm tra sau khi component đã render
+    const timer = setTimeout(checkHotspots, 2000);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <div className={styles.customizeCarPage}>
